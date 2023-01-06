a6a503cd0a4291541c0391b14d607708774075bf
zzlev <gpalektzja@gmail.com>
2023-01-06 22:47:46 +0900

    Add MemberRepositoryV0 findById.

--- src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -5,6 +5,7 @@ import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 @Slf4j
 public class MemberRepositoryV0 {
@@ -26,6 +27,36 @@ public class MemberRepositoryV0 {
         } finally {
             close(conn, pstmt, null);
         }
+    }
+
+    public Member findById(String memberId) throws SQLException {
+        String sql = "SELECT * FROM member WHERE member_id = ?";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found memberId=" + memberId);
+            }
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(conn, pstmt, rs);
+        }
+
     }
 
     private void close(Connection conn, Statement stmt, ResultSet rs) {
