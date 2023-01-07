ba1019ce89acffbda3261adbd72d7f8f6e0d4590
zzlev <gpalektzja@gmail.com>
2023-01-07 12:45:05 +0900

    Add MemberRepositoryV0 update and update test.

--- src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -2,10 +2,11 @@ package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 class MemberRepositoryV0Test {
@@ -15,12 +16,17 @@ class MemberRepositoryV0Test {
     @Test
     void crud() throws SQLException {
         // save
-        Member member = new Member("memberV0", 10000);
+        Member member = new Member("memberV5", 10000);
         repo.save(member);
 
         // findById
         Member foundMember = repo.findById(member.getMemberId());
         log.info("foundMember={}", foundMember);
-        Assertions.assertThat(foundMember).isEqualTo(member);
+        assertThat(foundMember).isEqualTo(member);
+
+        // update: money: 10000 -> 20000
+        repo.update(member.getMemberId(), 20000);
+        Member updatedMember = repo.findById(member.getMemberId());
+        assertThat(updatedMember.getMoney()).isEqualTo(20000);
     }
 }
