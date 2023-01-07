ba1c1e68a8e0bb1f2aea9bb0fbd1dbec62104d35
zzlev <gpalektzja@gmail.com>
2023-01-07 15:11:58 +0900

    Add ConnectionTest.

---
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -0,0 +1,36 @@
+package hello.jdbc.connection;
+
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import javax.sql.DataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+
+@Slf4j
+public class ConnectionTest {
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000);
+    }
+
+    private void useDataSource(DataSource dataSource) throws SQLException {
+        Connection conn1 = dataSource.getConnection();
+        Connection conn2 = dataSource.getConnection();
+        log.info("connection={}, class={}", conn1, conn1.getClass());
+        log.info("connection={}, class={}", conn2, conn2.getClass());
+    }
+}
