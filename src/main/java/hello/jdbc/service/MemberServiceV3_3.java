c372f20303e893f4575cc94d4810cd638b0503bf
zzlev <gpalektzja@gmail.com>
2023-01-09 23:12:35 +0900

    Add MemberServiceV3_3 and test.

---
+++ src/main/java/hello/jdbc/service/MemberServiceV3_3.java
@@ -0,0 +1,39 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.SQLException;
+
+@Slf4j
+public class MemberServiceV3_3 {
+
+    private final MemberRepositoryV3 memberRepository;
+
+    public MemberServiceV3_3(MemberRepositoryV3 memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Transactional
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        execute(fromId, toId, money);
+    }
+
+    private void execute(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validate(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private static void validate(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+
+}
